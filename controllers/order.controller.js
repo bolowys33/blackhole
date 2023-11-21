928d4782a96c82268fb39eca82f915ec00d87a8c
Olubodun <bolowys33@gmail.com>
2023-11-21 18:28:31 +0100

    aaa

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -266,6 +266,8 @@ async function getAllOrdersForVendor(req, res) {
                    continue
                 }
 
+                console.log(product);
+
                 const newDetails = {product_name: productDetails.name, quantity: product.quantity, price: product.price, category: product.category_id, image: product.images[0]}
                 productsArr.push(newDetails)
             }
